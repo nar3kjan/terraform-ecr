b9a657c645073120101c279c02d1627f0f659e81
Narek <zzz@zzz.zzz>
2022-11-18 00:04:05 +0400

    ddd

--- user_data.sh
+++ user_data.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-sudo apt-get update
-sudo snap install docker
-sudo addgroup --system docker
-sudo adduser ubuntu docker
-newgrp docker
-sudo snap disable docker
-sudo snap enable docker
-
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo unzip awscliv2.zip
-sudo ./aws/install
-
-sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 558664324013.dkr.ecr.us-east-1.amazonaws.com
-
-sudo docker run -d -p80:80 558664324013.dkr.ecr.us-east-1.amazonaws.com/ubuntu-nginx/main-*:latest
+IyEvYmluL2Jhc2gNCnN1ZG8gYXB0LWdldCB1cGRhdGUNCnN1ZG8gc25hcCBpbnN0YWxsIGRvY2tlcg0Kc3VkbyBhZGRncm91cCAtLXN5c3RlbSBkb2NrZXINCnN1ZG8gYWRkdXNlciB1YnVudHUgZG9ja2VyDQpuZXdncnAgZG9ja2VyDQpzdWRvIHNuYXAgZGlzYWJsZSBkb2NrZXINCnN1ZG8gc25hcCBlbmFibGUgZG9ja2VyDQoNCmN1cmwgImh0dHBzOi8vYXdzY2xpLmFtYXpvbmF3cy5jb20vYXdzY2xpLWV4ZS1saW51eC14ODZfNjQuemlwIiAtbyAiYXdzY2xpdjIuemlwIg0Kc3VkbyB1bnppcCBhd3NjbGl2Mi56aXANCnN1ZG8gLi9hd3MvaW5zdGFsbA0KDQpzdWRvIGF3cyBlY3IgZ2V0LWxvZ2luLXBhc3N3b3JkIC0tcmVnaW9uIHVzLWVhc3QtMSB8IGRvY2tlciBsb2dpbiAtLXVzZXJuYW1lIEFXUyAtLXBhc3N3b3JkLXN0ZGluIDU1ODY2NDMyNDAxMy5ka3IuZWNyLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tDQoNCnN1ZG8gZG9ja2VyIHJ1biAtZCAtcDgwOjgwIDU1ODY2NDMyNDAxMy5ka3IuZWNyLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tL3VidW50dS1uZ2lueC9tYWluLSo6bGF0ZXN0
