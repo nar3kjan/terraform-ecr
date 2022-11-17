85948e7dd4741bb65052af26cc67e56075a5db73
Narek <zzz@zzz.zzz>
2022-11-17 21:21:48 +0400

    token

--- ecr.tf
+++ ecr.tf
@@ -1,7 +1,8 @@
-module "ecr" {
+module "public_ecr" {
   source = "terraform-aws-modules/ecr/aws"
 
-  repository_name = "ubuntu-nginx"
+  repository_name = "ngnix-narek"
+  repository_type = "public"
 
   repository_lifecycle_policy = jsonencode({
     rules = [
@@ -21,9 +22,17 @@ module "ecr" {
     ]
   })
 
+  public_repository_catalog_data = {
+    description       = "Docker container for some things"
+    about_text        = file("${path.module}/files/ABOUT.md")
+    usage_text        = file("${path.module}/files/USAGE.md")
+    operating_systems = ["Linux"]
+    architectures     = ["x86"]
+    logo_image_blob   = filebase64("${path.module}/files/clowd.png")
+  }
+
   tags = {
     Terraform   = "true"
     Environment = "dev"
   }
 }
-
