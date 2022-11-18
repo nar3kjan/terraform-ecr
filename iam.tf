9e657c223bd429fa283402f33b495f03c8d2d143
Narek <zzz@zzz.zzz>
2022-11-18 16:34:22 +0400

    v1

--- iam.tf
+++ iam.tf
@@ -4,13 +4,13 @@ resource "aws_iam_role_policy" "ecr_policy" {
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
-  policy = var.iam_role_policy
+  policy = file("iamrolepolicy.json")
 }
 
 resource "aws_iam_role" "ecr_ec2_role" {
   name = "ecr-role"
 
-  assume_role_policy = var.assume_role_policy
+  assume_role_policy = file("iamassumepolicy.json")
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
